03efaaae94cb453d73a92b0fe413dd66a1658b16
Diogodhs <dhs@cin.ufpe.br>
2019-11-30 23:32:35 -0300

    2 sensores

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -5,28 +5,40 @@
 #define RX 0
 #define TX 10
 
- Ultrasonic ultrassom(8,7); // define o nome do sensor(ultrassom)
+Ultrasonic ultrassom1(8,7); // define o nome do sensor(ultrassom)
 //e onde esta ligado o trig(8) e o echo(7) respectivamente
- 
-long distancia;
- 
- void setup() {
- Serial.begin(9600); //Habilita Comunicação Serial a uma taxa de 9600 bauds.
- 
- }
- 
+Ultrasonic ultrassom2(6,5);
+
+long distancia1;
+long distancia2;
+
+void setup() {
+Serial.begin(9600); //Habilita Comunicação Serial a uma taxa de 9600 bauds.
+
+}
+
  void loop()
  {
-   distancia = ultrassom.Ranging(CM); // ultrassom.Ranging(CM) retorna a distancia em
-                                      // centímetros(CM) ou polegadas(INC)
-    if(distancia < 40){
-      Serial.println("Passou o lixo");
-      Serial.print(distancia); //imprime o valor da variável distancia
+    distancia1 = ultrassom1.Ranging(CM); // ultrassom.Ranging(CM) retorna a distancia em cm
+    distancia2 = ultrassom2.Ranging(CM);
+
+    if(distancia1 < 40){
+      Serial.println("Passou o lixo na entrada 1");
+      Serial.print(distancia1); //imprime o valor da variável distancia
       Serial.println("cm");
       digitalWrite(TX, LOW);  //Mensagem para a haspberry de que passou o lixo
     }else{
       digitalWrite(TX, HIGH); //Mensagem para a haspberry de que ainda não percebe nada no sensor
     }
-    
+
+    if(distancia2 < 40){
+      Serial.println("Passou o lixo na entrada 2");
+      Serial.print(distancia2); //imprime o valor da variável distancia
+      Serial.println("cm");
+      digitalWrite(TX, LOW);  //Mensagem para a haspberry de que passou o lixo
+    }else{
+      digitalWrite(TX, HIGH); //Mensagem para a haspberry de que ainda não percebe nada no sensor
+    }
+
     delay(100);
  }
